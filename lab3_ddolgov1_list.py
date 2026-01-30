c5be870255a5253288d0d5d71c134424111c37ca
Dmitrii Dolgov <ddolgov1@student.cscc.edu>
2026-01-30 13:56:32 -0500

    first commit. added sorted list

---
+++ lab3_ddolgov1_list.py
@@ -0,0 +1,14 @@
+"""
+List of items
+Author: Dmitrii Dolgov
+Purpose: prints out the list of things as is and in alphabetical order
+1/30/2026
+"""
+
+my_items=["tent","tent poles", "hammer", "swiss knife", "compass","map", 
+          "spare boots", "fishing rod", "tackle box", "book"]
+print(f"I have {len(my_items)} items in my list.")
+my_items.sort()
+print("here they are in alphabetical order:")
+for i in my_items:
+    print(i)
